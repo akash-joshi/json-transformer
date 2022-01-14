92816c1a8a899279b50bd711a80b69634904eb60
Akash Joshi <akash.joshi@viit.ac.in>
2022-01-14 11:39:45 +0530

    fix: add example

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@
 // applications without need of webpack (or other module bundler)
 // configuration files.
 
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import ReactJson from "react-json-view";
 import { useLocalStorage } from "react-use";
@@ -10,12 +10,22 @@ import { useLocalStorage } from "react-use";
 import Editor from "@monaco-editor/react";
 import { ClockLoader as Loader } from "react-spinners";
 
+import posts from "./posts.json";
+
 function App() {
   const [theme] = useState("light");
   const [language] = useState("javascript");
 
-  const [inputData, setInputData] = useLocalStorage("input", "");
-  const [codeData, setCodeData] = useLocalStorage("code", "");
+  const [inputData, setInputData] = useLocalStorage(
+    "input",
+    JSON.stringify(posts)
+  );
+  const [codeData, setCodeData] = useLocalStorage(
+    "code",
+    `const outputData = inputData.filter(post => post.userId === 10);
+    
+    outputData;`
+  );
   const [outputData, setOutputData] = useState("output", "");
 
   function handleInputChange(event) {
@@ -28,6 +38,21 @@ function App() {
     console.log("here is the current code value:", value);
     setCodeData(value);
   }
+
+  const evalCode = () => {
+    try {
+      const output = eval(`const inputData = ${inputData};
+  ${codeData}
+  `);
+      setOutputData(output);
+    } catch (error) {
+      setOutputData(error);
+    }
+  };
+
+  useEffect(() => {
+    evalCode();
+  }, []);
 
   return (
     <div
@@ -44,26 +69,12 @@ function App() {
         theme={theme}
         language={language}
         loading={<Loader />}
-        
         defaultValue={codeData}
         onChange={handleEditorChange}
       />
 
       <div>
-        <button
-          onClick={() => {
-            try {
-              const output = eval(`const inputData = ${inputData};
-          ${codeData}
-          `);
-              setOutputData(output);
-            } catch (error) {
-              setOutputData(error);
-            }
-          }}
-        >
-          Generate
-        </button>
+        <button onClick={evalCode}>Generate</button>
         {outputData && Object.keys(outputData)?.length ? (
           <ReactJson src={outputData} />
         ) : (
